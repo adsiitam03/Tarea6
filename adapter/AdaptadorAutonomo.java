01b08a220c1f7af2a94e817824d7ea7bc44eb8f1
adsiitam03 <36754977+adsiitam03@users.noreply.github.com>
2018-03-15 15:01:42 -0600

    adapter

---
+++ adapter/AdaptadorAutonomo.java
@@ -0,0 +1,18 @@
+public class AdaptadorAutonomo implements Chofer{
+
+  CocheAutonomo coche;
+
+  public AdaptadorAutonomo(CocheAutonomo c){
+    coche = c;
+  }
+
+  public void asignaCoche(String n){
+    System.out.println("Coche asignado con placas: "+coche.getPlacas());
+  }
+  public void conduceCoche(int m){
+    coche.seMueve(m);
+  }
+  public void estacionaCoche(String destino){
+    coche.seEstaciona(destino);
+  }
+}
