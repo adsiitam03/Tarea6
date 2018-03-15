8f6c4bda84f22d2f640c6064d11d5dbf577a05c9
adsiitam03 <36754977+adsiitam03@users.noreply.github.com>
2018-03-15 15:05:30 -0600

    visitor

---
+++ visitor/Dolar.java
@@ -0,0 +1,14 @@
+public class Dolar implements Visitable{
+  private double precio;
+  public Dolar(double p){
+    precio = p;
+  }
+
+  public double getPrecio(){
+    return precio;
+  }
+
+  public double accept(Visitor v){
+    return v.visit(this);
+  }
+}
