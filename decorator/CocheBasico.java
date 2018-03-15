3acacbd99fc916524f7ee8f73ac2e876fa277d77
adsiitam03 <36754977+adsiitam03@users.noreply.github.com>
2018-03-15 15:02:20 -0600

    decorator

---
+++ decorator/CocheBasico.java
@@ -0,0 +1,6 @@
+public class CocheBasico implements Coche{
+
+  public String info(){
+    return "Carroceria blanca, 4 llantas, 2 puertas";
+  }
+}
---
+++ decorator/PintaRojo.java
@@ -0,0 +1,13 @@
+public class PintaRojo extends DecoraCoche{
+
+  public PintaRojo(Coche c){
+    super(c);
+    System.out.println("Pintando");
+  }
+
+
+
+  public String info(){
+    return temp.info()+", Color rojo";
+  }
+}
